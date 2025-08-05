9d8e947119d2782d2735c03a944434367f646881
Keny R C Noleto <kenyrcnoleto@gmail.com>
2025-08-05 12:54:04 -0300

    API-1: when creating a new question, the status should be draft

--- tests/Feature/Question/StoreTest.php
+++ tests/Feature/Question/StoreTest.php
@@ -25,3 +25,22 @@ test('it should be albeto store a new question', function () {
     ]);
 
 });
+
+test('after create a new question, I nedde to make sure that it creates on _draft_ status', function () {
+    $user =  User::factory()->create();
+
+    // actingAs($user);
+
+    Sanctum::actingAs($user);
+
+    postJson(route('questions.store', [
+        'question' => 'Loren ipsun teste?'
+    ]))->assertSuccessful();
+
+    assertDatabaseHas('questions', [
+        'user_id'  => $user->id,
+        'status'    => 'draft',
+        'question' => 'Loren ipsun teste?'
+    ]);
+
+})->only();
