0e8c1c9c2aeabb9e71434739e31fafb4904f55f4
Keny R C Noleto <kenyrcnoleto@gmail.com>
2025-08-22 12:34:07 -0300

    API-13: archieve a question

---
+++ app/Http/Controllers/Question/ArchiveController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Question;
+
+use App\Http\Controllers\Controller;
+use App\Models\Question;
+use Illuminate\Http\Request;
+
+class ArchiveController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Question $question)
+    {
+        $this->authorize('archive', $question);
+
+        $question->delete();
+
+        return response()->noContent();
+    }
+}
