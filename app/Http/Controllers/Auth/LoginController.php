64b02a5fcbeed212441d153e54b432942d248260
Keny R C Noleto <kenyrcnoleto@gmail.com>
2025-09-05 14:10:53 -0300

    API-5: implementing login route

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        // $data = request()->validate([
+        //     'email' =>  ['required', 'email'],
+        //     'password'  =>  ['required'],
+        // ]);
+        // return response()->json($request->all());
+
+        if (!auth()->attempt($request->only('email', 'password'))) {
+
+            throw ValidationException::withMessages([
+                'email' => __('auth.failed'),
+            ]);
+
+        }
+
+        $request->session()->regenerate();
+
+        return response()->noContent();
+    }
+}
