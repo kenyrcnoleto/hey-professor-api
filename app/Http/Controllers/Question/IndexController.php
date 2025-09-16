2987ee4549949e12f86166b3728b3bbe9329c78d
Keny R C Noleto <kenyrcnoleto@gmail.com>
2025-09-16 11:34:06 -0300

    API-14: list all published questions

---
+++ app/Http/Controllers/Question/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Question;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\QuestionResource;
+use App\Models\Question;
+
+class IndexController extends Controller
+{
+    public function __invoke()
+    {
+        $questions = Question::query()
+                ->where('status', '=', 'published')
+                ->get();
+
+        return QuestionResource::collection($questions);
+    }
+}
