efdb0bbd7460653b3f5cadf8049eef125547843d
Keny R C Noleto <kenyrcnoleto@gmail.com>
2025-09-26 12:21:06 -0300

    API-22: listing all draft questions related to the logged user

--- tests/Feature/Question/MyQuestionsTest.php
+++ tests/Feature/Question/MyQuestionsTest.php
@@ -36,3 +36,35 @@ test('it should list only questions that the logged user has been created :: pub
     ]);
 
 });
+
+test('it should list only questions that the logged user has been created :: draft', function () {
+    $user = User::factory()->create();
+
+    Sanctum::actingAs($user);
+    $userQuestion        = Question::factory()->draft()->for($user)->create();
+    $anotherUserQuestion = Question::factory()->draft()->create();
+
+    //dd($userQuestion);
+    //my-questions/{status}
+
+    $request = getJson(route('my-questions', ['status' => 'draft']))
+        ->assertOk();
+
+    $request->assertJsonFragment([
+
+        'id'         => $userQuestion->id,
+        'question'   => $userQuestion->question,
+        'status'     => $userQuestion->status,
+        'created_by' => [
+            'id'   => $userQuestion->user->id,
+            'name' => $userQuestion->user->name,
+        ],
+        'created_at' => $userQuestion->created_at->format('Y-m-d h:i:s'),
+        'updated_at' => $userQuestion->updated_at->format('Y-m-d h:i:s'),
+        //TODO: add like and unlike count
+    ])->assertJsonMissing([
+        //'id' =>  $draft->id,
+        'question' => $anotherUserQuestion->question,
+    ]);
+
+});
