b1240a9d22f331c6321ada8001277c3ce1f85768
Keny R C Noleto <kenyrcnoleto@gmail.com>
2025-10-04 13:55:40 -0300

    API-17: making sure i can only use like or unlike to validate

--- tests/Feature/Question/VoteTest.php
+++ tests/Feature/Question/VoteTest.php
@@ -44,3 +44,19 @@ test('it should be able to unlike a question', function () {
         'unlike'      => true,
     ]);
 });
+
+test('it should guarentee that only the words like and unlike are been used to vote', function ($vote, $status) {
+    $user = Sanctum::actingAs(User::factory()->create());
+
+    $question = Question::factory()->published()->create();
+
+    postJson(route('question.vote', [
+        'question' => $question,
+        'vote'     => $vote,
+    ]))->assertStatus($status);
+
+})->with([
+    'like'    => ['like', 204],
+    'unlike'  => ['unlike', 204],
+    'invalid' => ['invalid', 422],
+]);
